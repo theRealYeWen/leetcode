72daa36f382b58a1b6a3c3916ad836eb52b2c8f9
Vin <ye.wen1@outlook.com>
2024-12-05 17:13:57 +0800

    ac lc704

---
+++ main/src/main/java/org/problems/Main.java
@@ -0,0 +1,13 @@
+package org.problems;
+
+import static org.problems.Problem01_Leetcode704.search;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+
+        search(nums, target);
+    }
+
+}
