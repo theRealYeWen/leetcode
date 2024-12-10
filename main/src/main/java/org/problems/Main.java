1e837eaf759bd1abbe353a5cfbb8b4db1d85f168
Vin <ye.wen1@outlook.com>
2024-12-10 18:29:52 +0800

    format file

--- main/src/main/java/org/problems/Main.java
+++ main/src/main/java/org/problems/Main.java
@@ -1,14 +1,13 @@
 package org.problems;
 
-import static org.problems.Problem03_Leetcode977.sortedSquares;
 import static org.problems.Problem04_Leetcode209.minSubArrayLen;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1,1,1,1,1,1,1,1};
+        int[] nums = {1, 1, 1, 1, 1, 1, 1, 1};
         int target = 11;
 
-        System.out.println(minSubArrayLen(target,nums));
+        System.out.println(minSubArrayLen(target, nums));
     }
 
 }
