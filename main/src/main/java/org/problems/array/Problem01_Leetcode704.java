c090bfeaaf4d05953cbdef60a7b8c3a21a022400
Vin <ye.wen1@outlook.com>
2025-02-25 01:51:04 +0800

    comment: add some comment to existing code

--- main/src/main/java/org/problems/array/Problem01_Leetcode704.java
+++ main/src/main/java/org/problems/array/Problem01_Leetcode704.java
@@ -2,24 +2,27 @@ package org.problems.array;
 
 public class Problem01_Leetcode704 {
     /*704. 二分查找
-力扣题目链接 https://leetcode.cn/problems/binary-search/
+    给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target，
+    写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
 
-给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
-示例 1:
-输入: nums = [-1,0,3,5,9,12], target = 9
-输出: 4
-解释: 9 出现在 nums 中并且下标为 4
+    示例 1:
+    输入: nums = [-1,0,3,5,9,12], target = 9
+    输出: 4
+    解释: 9 出现在 nums 中并且下标为 4
 
-示例 2:
-输入: nums = [-1,0,3,5,9,12], target = 2
-输出: -1
-解释: 2 不存在 nums 中因此返回 -1
+    示例 2:
+    输入: nums = [-1,0,3,5,9,12], target = 2
+    输出: -1
+    解释: 2 不存在 nums 中因此返回 -1
 
-提示：
-你可以假设 nums 中的所有元素是不重复的。
-n 将在 [1, 10000]之间。
-nums 的每个元素都将在 [-9999, 9999]之间。*/
-    public static int search(int[] nums, int target) {
+    提示：
+    你可以假设 nums 中的所有元素是不重复的。
+    n 将在 [1, 10000]之间。
+    nums 的每个元素都将在 [-9999, 9999]之间。*/
+
+    // 二分查找思路：每次取中间值，如果中间值大于目标值，则在左半边查找，如果中间值小于目标值，则在右半边查找
+    // 二分查找只适合有序的数组
+    public static int binarySearch(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
 
